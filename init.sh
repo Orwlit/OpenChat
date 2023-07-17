6eadbf01b25885da0e91f64f882d3860c71b0899
Orwlit <86476301+Orwlit@users.noreply.github.com>
2023-07-17 17:56:10 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -10,8 +10,4 @@ sudo sh test-docker.sh
 
 git clone https://github.com/openchatai/OpenChat.git
 cd OpenChat
-echo "OPENAI_API_KEY=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" > common.env
-echo "PINECONE_API_KEY=3721acd3-8514-47c9-8adc-1bd8e5d927e8" >> common.env
-echo "PINECONE_ENVIRONMENT=us-west1-gcp-free" >> common.env
-echo "PINECONE_INDEX_NAME=openchat" >> common.env
 make install
